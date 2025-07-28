bd7144d3a5a38901928aca8695aa1c10aef53071
mayankbhaiya03 <mayankbhaiya6@gmail.com>
2025-07-28 19:38:12 +0530

    Fix seed and environment DB connection

--- init/index.js
+++ init/index.js
@@ -1,29 +1,33 @@
+
+
 require("dotenv").config();
-
-const mongoose=require("mongoose");
-const initData=require("./data.js");
-const Listing=require("../models/listing.js");
-
-const dbUrl = process.env.ATLASDB_URL
-main().then(()=>{
-    console.log("connected to db");
-}).catch(err=>{
-    console.log(err);
-});
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
 
 
-async function main(){
-    await mongoose.connect(dbUrl);
+const dbUrl = process.env.ATLASDB_URL || "mongodb://127.0.0.1:27017/yelp-camp";
+
+async function main() {
+    try {
+        await mongoose.connect(dbUrl);
+        console.log("Connected to DB");
+
+        await Listing.deleteMany({});
+        initData.data = initData.data.map((obj) => ({
+            ...obj,
+            owner: "687f8cb63806233121ce41a9", 
+        }));
+
+        await Listing.insertMany(initData.data);
+        console.log("Data was initialized successfully.");
+
+      
+        await mongoose.disconnect();
+        console.log("Disconnected from DB");
+    } catch (err) {
+        console.error("Error during DB initialization:", err);
+    }
 }
 
-const initDB= async ()=>{
-    await Listing.deleteMany({});
-    initData.data=initData.data.map((obj)=>({
-        ...obj,
-        owner:'687f8cb63806233121ce41a9',
-    }));
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
-};
-
-initDB();
+main();
