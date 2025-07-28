68b9dd39592f7ff6ddacedfdd7e0aa12dba8eb5e
mayankbhaiya03 <mayankbhaiya6@gmail.com>
2025-07-28 19:03:04 +0530

    Fix: Seed data script connects to Atlas

--- init/index.js
+++ init/index.js
@@ -1,9 +1,10 @@
+require("dotenv").config();
+
 const mongoose=require("mongoose");
 const initData=require("./data.js");
 const Listing=require("../models/listing.js");
 
-const Mongo_url="mongodb://127.0.0.1:27017/wanderlust";
-
+const dbUrl = process.env.ATLASDB_URL
 main().then(()=>{
     console.log("connected to db");
 }).catch(err=>{
@@ -12,7 +13,7 @@ main().then(()=>{
 
 
 async function main(){
-    await mongoose.connect(Mongo_url);
+    await mongoose.connect(dbUrl);
 }
 
 const initDB= async ()=>{
